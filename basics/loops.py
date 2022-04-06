66029d37cb95b4a422b343474e171efce2ad2598
Prayas Pathak <prayasroman1@gmail.com>
2022-04-06 12:31:29 +0545

    learned about basic string manipulation.

--- basics/loops.py
+++ basics/loops.py
@@ -16,8 +16,40 @@
 # ****
 # *****
 
-i = 1
+# i = 1
 
-while(i <= 5):
-    print("*"*i)
-    i += 1
+# while(i <= 5):
+#     print("*"*i)
+#     i += 1
+
+
+#  For with range
+
+# for i in range(1, 20, 2):
+#     print(i)
+
+# Reversing a string
+
+name = "python programming"
+print(name[::-1])
+
+# for i in range(5):
+#     print(i)
+
+string = "HELLO World"
+vowels = ['a', 'e', 'i', 'o', 'u']
+count = 0
+for char in string.lower():
+    i = 0
+    if char in vowels:
+        count += 1
+print(f"Vowels = {count}")
+
+# new_string = ''
+# for i in range(len(string)-1, -1, -1):
+#     new_string += string[-i]
+# print(new_string)
+
+# new_string = ""
+new_string = "".join(reversed(string))
+print(new_string)
