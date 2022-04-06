57a89d32390c159b0d67bbeb1653e61ed42d61b1
Prayas Pathak <prayasroman1@gmail.com>
2022-04-06 11:39:02 +0545

    Optimized prime or composite logic.

--- basics/prime_or_composite.py
+++ basics/prime_or_composite.py
@@ -1,10 +1,24 @@
 number = int(input("Please enter a number."))
-i = 2
-while (i <= number):
-    if (number % i == 0):
-        break
+# i = 2
+# while (i <= number):
+#     if (number % i == 0):
+#         break
+#     i += 1
+# if (i == number):
+#     print(f"{number} is a prime number.")
+# else:
+#     print(f"{number} is not a prime number.")
+
+
+# Another version
+
+i = 1
+count = 0
+while i <= number:
+    if number % i == 0:
+        count += 1
     i += 1
-if (i == number):
+if count <= 2:
     print(f"{number} is a prime number.")
 else:
     print(f"{number} is not a prime number.")
