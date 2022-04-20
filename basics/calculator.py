4a7f61fdcc0b16dcff19e94d627225d683ecf0de
PrayashPathak <prayasroman1@gmail.com>
2022-04-20 19:48:38 +0545

    Added about calculator and user input.

--- basics/calculator.py
+++ basics/calculator.py
@@ -1,26 +1,87 @@
+print("Welcome to calculator: ")
 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+# Modularizing the code for code reusability
 
-choice = input("Enter '+' for addition, '-' for subtraction, '/' for division ,'*' for multiplication, '%' for modular division, '**' for exponentiation")
 
-if choice == '+':
-    print("You performed Addition. ")
-    print(f"{num1} + {num2} = {num1+num2}")
-elif choice == '-':
-    print("You perfomed subtraction.")
-    print(f'{num1}- {num2} = {num1-num2}')
-elif choice == '/':
-    print("You perfromed division.")
-    print(f"{num1} / {num2} = {num1/num2}")
-elif choice == '*':
-    print("You performed multiplication.")
-    print(f"{num1} * {num2} = {num1*num2}")
-elif choice == '%':
-    print("You performed modular division.")
-    print(f"{num1} % {num2} = {num1 % num2}")
-elif choice == '**':
-    print("You perormed exponentiation.")
-    print(f"{num1} ** {num2} = {num1**num2}")
-else:
-    print("Please provide a valid input.")
+def prompt():
+    choice = input("Select '*' for multiplication, '+' for addition, '-' for subtraction, '/' for division, '%' for modular division, '**' for exponentitaion and 'quit' to exit the program. ")
+
+    return choice
+
+
+def get_user_input():
+    num = int(input("Enter a number: "))
+    return num
+
+
+def select_operation(selection):
+    if selection == '+':
+        result = add(num1, num2)
+        print(f"{num1} + {num2} = {result}")
+    elif selection == '-':
+        result = subtract(num1, num2)
+        print(f"{num1} - {num2} = {result}")
+    elif selection == '*':
+        result = multiply(num1, num2)
+        print(f"{num1} * {num2} = {result}")
+    elif selection == '/':
+        result = divide(num1, num2)
+        print(f"{num1} / {num2} = {result}")
+    elif selection == '%':
+        result = modular_division(num1, num2)
+        print(f"{num1} % {num2} = {result}")
+    elif selection == '**':
+        result = exponentiation(num1, num2)
+        print(f"{num1} ** {num2} = {result}")
+    else:
+        print("Invalid Selection made.")
+
+
+def wants_to_continue():
+    response = input(
+        "Press 'y' to continue and 'n' to termintate the program: ").lower()
+    if response == 'y':
+        return True
+    return False
+
+
+def add(num1, num2):
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    return num1 - num2
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def divide(num1, num2):
+    return num1 // num2
+
+
+def modular_division(num1, num2):
+    return num1 % num2
+
+
+def exponentiation(number, exponent):
+    return number ** exponent
+
+
+can_continue = True
+
+
+while can_continue:
+    choice = prompt()
+    num1 = get_user_input()
+    num2 = get_user_input()
+    select_operation(choice)
+    if wants_to_continue():
+        can_continue = True
+        choice = prompt()
+        num1 = get_user_input()
+        num2 = get_user_input()
+        select_operation(choice)
+    else:
+        can_continue = False
