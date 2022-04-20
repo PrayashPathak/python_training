c79b8556e6166b940da2ffb7a9a1e5e40639906d
PrayashPathak <prayasroman1@gmail.com>
2022-04-20 12:28:25 +0545

    To do exercise.

---
+++ basics/variable_input.py
@@ -0,0 +1,18 @@
+# times = int(input("Enter the number of times the addition should be performed: "))
+# total = 0
+# for i in range(times):
+#     num = int(input("Enter a number to perform addition: "))
+#     total += num
+# print(f"Total = {total}")
+
+# While loop implementation
+times = int(input("Enter the number of times the addition should be performed: "))
+total = 0
+i = 0
+while(i != times):
+    num = int(input("Enter the number to perform addition: "))
+    total += num
+    i += 1
+print(f"Total = {total}")
+
+# Make a continuous calculator.
