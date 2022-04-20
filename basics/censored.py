4f5dc3cfb98f7fc6772863836a614a8c03d5a6d0
PrayashPathak <prayasroman1@gmail.com>
2022-04-20 12:08:24 +0545

    demo profanity filter.

--- basics/censored.py
+++ basics/censored.py
@@ -1,35 +1,50 @@
+# from better_profanity import profanity  # For censoring words
 
-from better_profanity import profanity  # For censoring words
+# file = open('swear.txt', 'r')
+# user_input = input("Enter a text: ")
+# swear_words = []
+# swear_words.append(file.read())
+# file.close()
 
-file = open('swear.txt', 'r')
-user_input = input("Enter a text: ")
-swear_words = []
-swear_words.append(file.read())
-file.close()
+# # Writing to a file
+# file = open("swear.txt", "w")
+# file.write(user_input)
+# file.close()
 
-# Removing the escape sequences from the list
+# # Removing the escape sequences from the list
 
-swear_list = " ".join(swear_words)
-escapes = ''.join([chr(char) for char in range(1, 32)])
-translator = str.maketrans('', '', escapes)
-swear_words = swear_list.translate(translator)
+# swear_list = " ".join(swear_words)
+# escapes = ''.join([chr(char) for char in range(1, 32)])
+# translator = str.maketrans('', '', escapes)
+# swear_words = swear_list.translate(translator)
 
-print(swear_words)
+# print(swear_words)
 
-# Adding custom swear words
-custom_swear_words = profanity.load_censor_words_from_file('./swear.txt')
-print(custom_swear_words)
+# # Adding custom swear words
+# custom_swear_words = profanity.load_censor_words_from_file('./swear.txt')
+# print(custom_swear_words)
 
-censored = ""
+# censored = ""
 
-# Censors the swear word completely.
+# # Censors the swear word completely.
 
-if profanity.contains_profanity(user_input):
-    censored += profanity.censor(user_input)
-    print(censored)
-else:
-    print(user_input)
+# if profanity.contains_profanity(user_input):
+#     censored += profanity.censor(user_input)
+# else:
+#     print(user_input)
 
-# if user_input in custom_swear_words:
-#     censored += user_input
-#     print(censored)
+
+# Simple logic
+
+fin = open('swear.txt', 'r')
+data = fin.read()
+bad_words = ["this", "line", "make", "demo", "censoring"]
+
+for word in bad_words:
+    data = data.replace(word, "*"*len(word))
+
+fout = open("output.txt", "w")
+fout.write(data)
+
+fout.close()
+fin.close()
